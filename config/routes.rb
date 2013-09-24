64abc21289495ecadd86164ef427627ca9df28b5
Luke Rasmussen <luke.rasmussen@northwestern.edu>
2013-09-24 08:05:32 -0500

    Default to the new submission page

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ EmergeDataValidation::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'submissions#new'
 
   # See how all your routes lay out with "rake routes"
 
