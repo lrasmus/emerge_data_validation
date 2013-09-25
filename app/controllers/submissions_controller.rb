ef533771528e9f0136c604b095cfc1992e4f0600
Luke Rasmussen <luke.rasmussen@northwestern.edu>
2013-09-24 22:24:33 -0500

    Provide a basic UI for submitting & viewing results

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,6 @@
-#require 'validation/file_processor'
-#require 'validation/data_dictionary_validator'
+require 'validation/file_processor'
+require 'validation/data_dictionary_validator'
+require 'validation/data_file_validator'
 
 class SubmissionsController < ApplicationController
   # GET /submissions
@@ -44,21 +45,31 @@ class SubmissionsController < ApplicationController
   # POST /submissions.json
   def create
     @submission = Submission.new(params[:submission])
-    #data_dictionary = params[:submission][:data_dictionary].read
-    #data_file = params[:submission][:data_file].read
+    data_dictionary = params[:submission][:data_dictionary].read
+    data_file = params[:submission][:data_file].read unless params[:submission][:data_file].blank?
 
-    #dd_processor = EMERGE::Phenotype::DataFileProcessor.new data_dictionary, :dictionary
-    #dd_processor.process
-
-    respond_to do |format|
-      if @submission.save
-        format.html { redirect_to @submission, notice: 'Submission was successfully created.' }
-        format.json { render json: @submission, status: :created, location: @submission }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @submission.errors, status: :unprocessable_entity }
+    unless data_dictionary.blank?
+      dd_processor = EMERGE::Phenotype::DataDictionaryValidator.new data_dictionary, :csv
+      @data_dictionary_results = dd_processor.validate
+      @data_dictionary_results[:file_name] = params[:submission][:data_dictionary].original_filename
+      unless data_file.blank?
+        df_processor = EMERGE::Phenotype::DataFileValidator.new data_file, dd_processor.variables, :csv
+        @data_file_results = df_processor.validate
+        @data_file_results[:file_name] = params[:submission][:data_file].original_filename
       end
     end
+
+    render "report"
+
+    #respond_to do |format|
+    #  if @submission.save
+    #    format.html { redirect_to @submission, notice: 'Submission was successfully created.' }
+    #    format.json { render json: @submission, status: :created, location: @submission }
+    #  else
+    #    format.html { render action: "new" }
+    #    format.json { render json: @submission.errors, status: :unprocessable_entity }
+    #  end
+    #end
   end
 
   # PUT /submissions/1
